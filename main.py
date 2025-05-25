6ed06127eb572a0294f7a9436cb336af2b9a1633
JesusPetro <petrojesus094@gmail.com>
2025-05-25 00:58:29 -0500

    middleware cors

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 origin = [
     'http://localhost:5173',
-    'https://backend-chayarm.onrender.com'
+    'https://hmi-chayarm-c9iqr11sb-gsus-projects-b0d1512e.vercel.app'
 ]
 
 app.add_middleware(
