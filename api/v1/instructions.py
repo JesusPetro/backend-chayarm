2215a829d9423bb9eb598eb63e6dcae3114f3f35
JesusPetro <petrojesus094@gmail.com>
2025-06-01 17:15:38 -0500

    configuracion de steps

--- api/v1/instructions.py
+++ api/v1/instructions.py
@@ -16,8 +16,7 @@ commands = {
             'blue': 'search_head_b',
             'red': 'search_head_c'
         },
-        'put': 'put_head',
-        'close_gripper': 'close_gripper_head'
+        'put': 'put_head'
     },
     'body': {
         'search': {
@@ -25,8 +24,7 @@ commands = {
             'blue': 'search_body_b',
             'red': 'search_body_c'
         },
-        'put': 'put_body',
-        'close_gripper': 'close_gripper_body'
+        'put': 'put_body'
     },
     'leg': {
         'search': {
@@ -34,19 +32,27 @@ commands = {
             'blue': 'search_leg_b',
             'red': 'search_leg_c'
         },
-        'put': 'put_leg',
-        'close_gripper': 'close_gripper_leg'
+        'put': 'put_leg'
     },
     'gripper': {
-        'open': 'open_gripper'
-    }
+        'open': 'open_gripper',
+        'close': 'close_gripper'
+    },
+    'neutral': 'neutral_position'
 }
+
+neutral = get_instruction_angles(instruction=commands['neutral'])
+gripper_close = get_instruction_angles(instruction=commands['gripper']['close'])
+gripper_open = get_instruction_angles(instruction=commands['gripper']['open'])
+    
 
 @router.get('/stept_assemble/')
 async def get_steps_to_assemble(head: Optional[str] = 'white', body: Optional[str] = 'white', leg: Optional[str] = 'white'):
-    steps = [] + get_instruction_angles(instruction=commands['head']['search'][head])
-    steps += get_instruction_angles(instruction=commands['head']['close_gripper'])
-    steps += get_instruction_angles(instruction=commands['body']['search'][body])
-    steps += get_instruction_angles(instruction=commands['leg']['search'][leg])
+    
+    steps = []
+    steps += get_instruction_angles(instruction=commands['leg']['search'][leg])   + gripper_close + neutral + get_instruction_angles(instruction=commands['leg']['put'])  + gripper_open
+    steps += get_instruction_angles(instruction=commands['body']['search'][body]) + gripper_close + neutral + get_instruction_angles(instruction=commands['body']['put']) + gripper_open
+    steps += get_instruction_angles(instruction=commands['head']['search'][head]) + gripper_close + neutral + get_instruction_angles(instruction=commands['head']['put']) + gripper_open
+    steps += neutral
     return steps
 
